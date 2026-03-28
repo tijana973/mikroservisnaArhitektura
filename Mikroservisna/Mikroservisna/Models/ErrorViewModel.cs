dd260922ee075475536a304159ce323d6af4757e
Tijana <tijanaboskovic973@gmail.com>
2026-03-28 14:07:55 +0100

    Drugi commit

---
+++ Mikroservisna/Mikroservisna/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mikroservisna.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
